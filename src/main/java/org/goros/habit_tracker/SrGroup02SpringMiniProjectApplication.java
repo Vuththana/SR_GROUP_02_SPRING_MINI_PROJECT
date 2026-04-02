c57c64f9f56841b383af3467ac41a10f6d6601d1
Vuththana <vuththanakeo69@gmail.com>
2026-04-02 13:23:31 +0700

    first commit

---
+++ src/main/java/org/goros/habit_tracker/SrGroup02SpringMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package org.goros.habit_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrGroup02SpringMiniProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrGroup02SpringMiniProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/goros/habit_tracker/SrGroup02SpringMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.goros.habit_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrGroup02SpringMiniProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

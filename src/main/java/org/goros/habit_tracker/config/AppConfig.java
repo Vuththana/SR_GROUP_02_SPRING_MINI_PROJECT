7090581252fda7c053e3b7528f10bfc96a0b94ad
Vuththana <vuththanakeo69@gmail.com>
2026-04-03 22:34:11 +0700

    .env working, done with send and verify

--- src/main/java/org/goros/habit_tracker/config/AppConfig.java
+++ src/main/java/org/goros/habit_tracker/config/AppConfig.java
@@ -3,6 +3,7 @@ package org.goros.habit_tracker.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
